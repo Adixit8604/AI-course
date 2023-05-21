71f0f594be6356d1cabf1eb7159eee15fed84ee0
Adixit8604 <92964566+Adixit8604@users.noreply.github.com>
2023-05-21 13:02:52 +0530

    21-may-2022-1-02

---
+++ Code/Basic/print.py
@@ -0,0 +1,7 @@
+print("Hello World")
+#print("Hello World"  gives error
+print(20+50) #int addition
+print("20+50") #string
+print('20+50') #string
+print("Hello","dude",56) #multiple statement in one print function
+print(10>5) #output is boolean value
---
+++ Code/Basic/variable.py
@@ -0,0 +1,12 @@
+a=10
+print(a)
+a=20
+b=20
+print(a,b)
+print(id(a),id(b))
+print("a") #treated as a string
+c=20
+d=30
+print(id(c),id(d))
+e="hello"
+print(e)
